934487f2639f512302a74d1ddc6cd9001cbcd94e
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-25 10:36:55 -0500

    initial commit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args){
+        // Question 1:
+        String favoriteColor = "blue";
+        System.out.println("My favorite color is " + favoriteColor);
+
+
+
+    }
+}
