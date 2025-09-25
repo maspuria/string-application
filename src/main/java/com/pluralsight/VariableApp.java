8f923b9786adec8664c12759f2c9724eb23bfd90
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-25 11:03:16 -0500

    step 1 and 2 commit

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -8,6 +8,33 @@ public class VariableApp {
         // Question 2:
         int startYear = 2025;
         System.out.println("The year I started this class is " + startYear);
+        // Question 3:
+        char middleInitial = 'P';
+        System.out.println("My middle initial is " + middleInitial);
+        // Question 4:
+        boolean hasPets = true;
+        System.out.println("I have pets: " + hasPets);
+        // Question 5:
+        String niceMessage = "You can achieve difficult tasks!";
+        System.out.println("Message of the day: " + niceMessage);
+        // Step 2
+        // Question 1: Declare AND initialize variables for:
+        // days in the week
+        int daysInTheWeek = 7;
+        System.out.println("The amount of days in a week: " + daysInTheWeek);
+        //price of the coffee
+        float costOfCoffee = 2.99f;
+        System.out.println("The cost of coffee is " + costOfCoffee);
+        // your favorite letter
+        char favLetter = 'M';
+        System.out.println("My favorite letter is: " + favLetter);
+        // whether it is raining (true or false)
+        boolean raining = false;
+        System.out.println("It is raining: " + raining);
+        //
+
+
+
 
 
 
