2feff9487589566406174ad8b72dc186f3c59936
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-25 10:41:21 -0500

    question 2 commit

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -5,6 +5,10 @@ public class VariableApp {
         // Question 1:
         String favoriteColor = "blue";
         System.out.println("My favorite color is " + favoriteColor);
+        // Question 2:
+        int startYear = 2025;
+        System.out.println("The year I started this class is " + startYear);
+
 
 
 
