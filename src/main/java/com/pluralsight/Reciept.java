35429db1dcec159d0e8ed6e8bfd6144da95d6436
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-25 11:15:54 -0500

    update reciept commit

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] args) {
+        // create variables for an item(String) and item price(double)
+        String product = "Sonny Angels";
+        Double price = 11.99;
+        int amount = 2;
+        System.out.println("I bought " + amount + " " + product + " " + "for " + "$" + price);
+    }
+}
