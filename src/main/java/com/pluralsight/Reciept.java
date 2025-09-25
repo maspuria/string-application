2f6d059eb3e437422298b123752273dbaf76bace
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-25 11:42:42 -0500

    line 7 commit

--- src/main/java/com/pluralsight/Reciept.java
+++ src/main/java/com/pluralsight/Reciept.java
@@ -3,8 +3,8 @@ package com.pluralsight;
 public class Reciept {
     public static void main(String[] args) {
         // create variables for an item(String) and item price(double)
-        String product = "Sonny Angels";
-        double price = 11.99;
+        String product = "Smiski blind boxes";
+        double price = 7.25;
         int amount = 2;
         System.out.println("I bought " + amount + " " + product + " " + "for " + "$" + price);
     }
